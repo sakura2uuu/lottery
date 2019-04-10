91abbbf685520c4e141d7746393e11cdc40b0894
Wang Zhenyan <sakura2u@qq.com>
2019-04-10 11:50:49 +0800

    Adding rewards

---
+++ js/rewards.js
@@ -0,0 +1,47 @@
+var rewards = [
+  {
+    name: 'Supergoop! SuperScreen Daily Moisturizer SPF 50 (1.5ml)',
+    image: 'https://static-reg.lximg.com/images/product_images/default_1_GWP_206097-Supergoop-SuperScreen-Daily-Moisturizer-SPF-50-1-5ml-Default_730b0d72a6b6e3eaf9c1e88527809c3c8dd924f5_1548411374.png',
+    tier: 3
+  },
+  {
+    name: 'Smashbox Always On LL (Bawse)',
+    image: 'https://static-reg.lximg.com/images/product_images/featured_Smashbox_AlwaysOnLiquidLipstickBawse_GWP_WEB_985ab892341093080502025b732e373fbdcff793_1523784992.png',
+    tier: 3
+  },
+  {
+    name: 'Smashbox Primed & Ready Set',
+    image: 'https://static-reg.lximg.com/images/product_images/featured_SephoraBeautyPass_SmashboxPrimeReady_GWP_WEB_2392a604f29f46dd902c41eb95a4b359f33b98d3_1523792279.png',
+    tier: 2
+  },
+  {
+    name: 'Sigma Beauty Sigma Spa Brush Cleaning Mat',
+    image: 'https://catrina-production.s3-ap-southeast-1.amazonaws.com/uploads/variant_assets/Sigma_DeluxeSpaMatSample_GWP_WEB_40cbb84c5fdf4cde723b8e97818e475c99cbeaeb_1523784875.jpg',
+    tier: 2
+  },
+  {
+    name: 'Tatcha The Water Cream (5ml)',
+    image: 'https://catrina-production.s3-ap-southeast-1.amazonaws.com/uploads/variant_assets/TheWaterCream_1000x1000_fceb42c736c18b27cfabf59ddf4266d3dd8c3103_1526975534.jpg',
+    tier: 2
+  },
+  {
+    name: 'Versace Dylan Blue Pour Femme Eau De Parfum Minature (5ml) ',
+    image: 'https://catrina-production.s3-ap-southeast-1.amazonaws.com/uploads/variant_assets/702062%20VDPF%20Miniatures_a22a96d4b5dcb41a704a22d5bf73617427aae110_1541131156.jpg',
+    tier: 2
+  },
+  {
+    name: 'Sephora Collection It Is Never Too Much! Lip Palette',
+    image: 'https://catrina-production.s3-ap-southeast-1.amazonaws.com/uploads/variant_assets/Sephora%252520Collection%252520It%252520Is%252520Never%252520Too%252520Much%252521%252520Lip%252520Palette%2525201_2e4b257928529b0c06069f7c70b56af553460f6b_1542277673.jpg',
+    tier: 2
+  },
+  {
+    name: '20% OFF Discount Coupon',
+    image: 'https://catrina-production.s3-ap-southeast-1.amazonaws.com/uploads/variant_assets/featured_300point_10percent_checkout_95a2371fadf306cb0739fae3be80a083781839d0_1523784656.jpg',
+    tier: 1
+  },
+  {
+    name: 'Exclusive Meet & Greet with Rihanna',
+    image: 'https://static-reg.lximg.com/images/billboards/SundayRiley_Powercouple_smallBB_cafc8d879837c792d66245c8e691763d841b551e_1554434164.jpg',
+    tier: 1
+  }
+]
