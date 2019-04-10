28f5cc58831ad648c20739cb64965ab875ff9f38
Truc Kieu <kieudongoctruc@gmail.com>
2019-04-10 13:33:45 +0800

    adding Points seed

---
+++ js/points.js
@@ -0,0 +1,21 @@
+const points = {
+  200: {
+    posibilityForTiers: {
+      2: 0.20,
+      3: 0.50
+    }
+  },
+  300: {
+    posibilityForTiers: {
+      1: 0.03,
+      2: 0.30,
+      3: 0.80
+    },
+  },
+  500: {
+    posibilityForTiers: {
+      1: 0.05,
+      2: 0.50
+    },
+  }
+}
