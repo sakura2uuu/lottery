df1c4e7ba3046450067bca62ead646d9b03c911c
Truc Kieu <kieudongoctruc@gmail.com>
2019-04-10 18:32:47 +0800

    Winning logic

--- js/points.js
+++ js/points.js
@@ -1,21 +1,21 @@
-const points = {
+var points = {
   200: {
     possibilityForTiers: {
-      2: 0.20,
-      3: 0.50
+      2: 0.40,
+      3: 0.60
     }
   },
   300: {
     possibilityForTiers: {
-      1: 0.03,
-      2: 0.30,
-      3: 0.80
+      1: 0.05,
+      2: 0.45,
+      3: 0.50
     },
   },
   500: {
     possibilityForTiers: {
-      1: 0.05,
-      2: 0.50
+      1: 0.20,
+      2: 0.80
     },
   }
 }
